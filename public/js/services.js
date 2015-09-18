f8573321569b7dc6a9154001e1dc0dede444bbbf
Gigozzo <Gigozzo4@gmail.com>
2015-09-18 16:27:54 +0300

    Realize /groups/:id/

--- public/js/services.js
+++ public/js/services.js
@@ -6,7 +6,8 @@ var vkcomparerServices = angular.module('vkcomparerServices', ['ngResource']);
 
 vkcomparerServices.factory('Group', ['$resource',
   function($resource){
-    return $resource('groups/:groupId.json', {}, {
+//    return $resource('groups/:groupId.json', {}, {
+    return $resource('api/groups/:groupId', {}, {
       query: {method:'GET', params:{groupId:'groups'}, isArray:true}
     });
   }]);
--- server.js
+++ server.js
@@ -60,14 +60,10 @@ app.get('/api/groups', function (req, res) {
 	});
 });
 
-// Получить массив с ИДшниками членов группы
-app.get('/api/groups/:id/members', function (req, res) {
-	res.send('Members IDs here');
-});
-
 // Получить инфу о группе
 app.get('/api/groups/:id', function (req, res) {
-	log.info("I want group INFO about " + req.params.id);
+//	res.send('Group INFO here');
+log.info("I want Group INFO " + req.params.id);
 //	return GroupModel.findById(req.params.id, function (err, group) {
 console.log('Start... ' + req.params.id + ' and type is ' + typeof req.params.id);
 //	return GroupModel.find({ $or: [{id: req.params.id}, {screen_name: req.params.id}]}, function (err, group) {
@@ -104,7 +100,147 @@ console.log('Start... ' + req.params.id + ' and type is ' + typeof req.params.id
 
 				if (members_count > 120000) {
 					res.statusCode = 400;
-					return res.send('Too large members count. Now we can work with groups with members_count less than 120k. Sorry.');
+					return res.send('Too large members count. Now we can work with groups where members_count less than 120k. Sorry.');
+				}
+
+				var i = 0;
+				while (i < _o.response[0].members_count) {
+//					console.log('i = ' + i);
+
+					vk.request('groups.getMembers', {'group_id' : group_id, 'offset': i, count: 1000}, function(_o) {
+						sum_count+=_o.response.items.length;
+						members = members.concat(_o.response.items);
+
+console.log('members count = ' + members.length);
+
+						if (--key === 0) {
+							console.log('<<<<===>DONE_DONE_DONE>>>===');
+
+							var group = new GroupModel({
+								id: group_id,
+								url: group_id,
+								screen_name: screen_name,
+								members_count: members_count,
+								name: name,
+								is_closed: is_closed,
+								description: 'desription',
+								members: members
+//								members: [1,2,3]
+                            });
+
+/////////////////////////////////////////////////////////////////////////////////////
+							return res.send({
+								id: group_id,
+								url: group_id,
+								screen_name: screen_name,
+								members_count: members_count,
+								name: name,
+								is_closed: is_closed,
+								description: 'desription',
+							});
+/////////////////////////////////////////////////////////////////////////////////////
+
+    group.save(function (err) {
+        if (!err) {
+            console.log("group created!");
+//            return res.send({ status: 'OK', group:group });
+        } else {
+            console.log(err);
+            if(err.name == 'ValidationError') {
+//                res.statusCode = 400;
+//                res.send({ error: 'Validation error' });
+				console.log('Validation error')
+            } else {
+//                res.statusCode = 500;
+//                res.send({ error: 'Server error' });
+				console.log('Server error');
+            }
+            console.log('error id: ' + res.statusCode,err.message);
+//            log.error('Internal error(%d): %s',res.statusCode,err.message);
+        }
+    });
+//							console.log(members);
+/////////////////////////////////////////////////////////////////////////////////////
+//							return res.send(members);
+/////////////////////////////////////////////////////////////////////////////////////
+						}
+//						console.log('(key) = ' + key);
+
+					});
+
+					i+=1000;
+				};
+			});
+
+		} else {
+			console.log('I find this DataBase!!!');
+//			console.log(group);
+			console.log('Name: ' + group[0].name);
+			return res.send({
+								id: group[0].id,
+								url: group[0].url,
+								screen_name: group[0].screen_name,
+								members_count: group[0].members_count,
+								name: group[0].name,
+								is_closed: group[0].is_closed,
+								description: group[0].description,
+							});
+/////////////////////////////////////////////////////////////////////////////////////
+//			return res.send(group[0].members);
+/////////////////////////////////////////////////////////////////////////////////////
+		}
+		console.log('<<<ERROR>>>');
+		console.log(err);
+//		if (!err) {
+//			return res.send({ status: 'OK', article:article });
+//		} else {
+//			res.statusCode = 500;
+//			log.error('Internal error(%d): %s',res.statusCode,err.message);
+//			return res.send({ error: 'Server error' });
+//		}
+	});
+});
+
+// Получить массив с ИДшниками членов группы
+app.get('/api/groups/:id/members', function (req, res) {
+	log.info("I want group Members IDs" + req.params.id);
+//	return GroupModel.findById(req.params.id, function (err, group) {
+console.log('Start... ' + req.params.id + ' and type is ' + typeof req.params.id);
+//	return GroupModel.find({ $or: [{id: req.params.id}, {screen_name: req.params.id}]}, function (err, group) {
+	return GroupModel.find({screen_name: req.params.id}, function (err, group) {
+		console.log('req.params.id>>> ' + req.params.id);
+//		console.log(group);
+		if(!group.length) {
+			log.info('Группа в БД не обнаружена');
+			log.error(err);
+			var vk = new VK({
+				'appId'     : 4744452,
+				'appSecret' : 'RzpMCpbjiomPF5sKBSh4',
+				'language'  : 'ru'
+			});
+
+			var sum_count = 0;
+
+			vk.request('groups.getById', {'group_ids' : req.params.id, 'fields': 'members_count'}, function(_o) {
+//				log.info('DONE');
+				console.log(_o.response);
+				console.log(_o.response[0].id);
+				console.log(_o.response[0].members_count);
+
+				sum_count+=0;
+				var
+					members = [],
+					key = (_o.response[0].members_count / 1000 | 0) + 1,
+					group_id = _o.response[0].id,
+//					group = _o.response[0];
+					screen_name = _o.response[0].screen_name,
+					members_count = _o.response[0].members_count,
+					name = _o.response[0].name,
+					is_closed = _o.response[0].is_closed;
+
+				if (members_count > 120000) {
+					res.statusCode = 400;
+					return res.send('Too large members count. Now we can work with groups where members_count less than 120k. Sorry.');
 				}
 
 				var i = 0;
